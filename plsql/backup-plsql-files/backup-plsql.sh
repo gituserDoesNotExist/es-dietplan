ea5608ae2068eb3e58a2c40f9d87e670a6b66227
gituserDoesNotExist <manfred-beutel@web.de>
2019-03-10 15:21:23 +0100

    backup plsql files at So 10 Mär 2019 15:21:13 CET

--- plsql/backup-plsql-files/backup-plsql.sh
+++ plsql/backup-plsql-files/backup-plsql.sh
@@ -9,7 +9,7 @@ scheme2dllJarName='scheme2ddl-2.4.3-SNAPSHOT.jar'
 currentDate=$(date +'%c')
 
 echo $logInfo 'starting oracle db'
-service oracle-xe start
+su -c "service oracle-xe start"
 
 
 echo $logInfo $currentDate: 'backing up schema"' $schema '"to "'$backupBaseDir'"'

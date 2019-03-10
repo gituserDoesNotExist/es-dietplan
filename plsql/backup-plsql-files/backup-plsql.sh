4abcc17938a8c83443065eebab7d0d8bd4770e4b
gituserDoesNotExist <manfred-beutel@web.de>
2019-03-10 14:44:04 +0100

    backup plsql files at dd.mm.yyyy

---
+++ plsql/backup-plsql-files/backup-plsql.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+logInfo='backup-plsql.sh: '
+
+schema='playground'
+password='playground'
+backupBaseDir=$(pwd)
+scheme2dllJarName='scheme2ddl-2.4.3-SNAPSHOT.jar'
+currentDate=$(date +'dd.mm.yyyy')
+
+echo $logInfo $currentDate: 'backing up schema"' $schema '"to "'$backupBaseDir'"'
+
+java -jar $scheme2dllJarName -url $schema/$password@localhost:1521:xe -o $backupBaseDir
+
+
+echo $logInfo 'pushing to origin develop'
+
+git checkout develop
+git add .
+git commit -m 'backup plsql files at '$currentDate 

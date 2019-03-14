be415bdd39b11d34f5a3c47f695d55e76cc80b15
gituserDoesNotExist <manfred-beutel@web.de>
2019-03-14 23:25:09 +0100

    message

--- plsql/backup-plsql-files/backup-dietplan.sh
+++ plsql/backup-plsql-files/backup-dietplan.sh
@@ -10,6 +10,7 @@ echo "$logInfo start to back up files" >> $logFileName
 jarName='scheme2ddl-2.4.3-SNAPSHOT.jar'
 java -jar $jarName >> $logFileName
 
+git checkout develop
 git add .
 git commit --author="ManfredBeutel <manfred-beutel@web.de>" -m "backed up files at $currentDate"
 git push https://gituserDoesNotExist:741eba1b@github.com/gituserDoesNotExist/es-dietplan.git

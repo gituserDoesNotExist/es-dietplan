7bdae24e90796f90830a79bb097e2e21c2b74d57
gituserDoesNotExist <manfred-beutel@web.de>
2019-03-16 01:19:58 +0100

    backed up files at Sa 16 Mär 2019 01:19:55 CET

--- plsql/backup-plsql-files/DIETPLAN/package_bodies/api.sql
+++ plsql/backup-plsql-files/DIETPLAN/package_bodies/api.sql
@@ -8,10 +8,19 @@ CREATE OR REPLACE PACKAGE BODY "DIETPLAN"."API" AS
         brennstoffe_kohlenhydrate varchar2
         
     ) AS
-    next_id FOOD.ID%TYPE;
+    next_food_id FOOD.ID%TYPE;
+    next_brennstoffe_id BRENNSTOFFE.ID%TYPE;
   BEGIN
-    next_id := SEQ_FOOD.nextval;
-    dbms_output.put_line('next id =' || next_id);
+    dbms_output.put_line('inserting into tables');
+    
+    next_food_id := SEQ_FOOD.nextval;
+    next_brennstoffe_id := SEQ_BRENNSTOFFE.nextval;
+    
+    insert into food(id,name) values(next_food_id,food_name);
+    insert into brennstoffe(id,fett,eiweiss,kohlenhydrate,food_id) values (next_brennstoffe_id,brennstoffe_fett,brennstoffe_eiweiss,brennstoffe_kohlenhydrate,next_food_id);
+    
+    COMMIT;
+    
   END create_complete_food;
 
 END API;

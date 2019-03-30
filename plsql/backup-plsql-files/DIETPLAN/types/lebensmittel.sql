7523a933eb8177269184c9a8beb876ebc9556a12
gituserDoesNotExist <manfred-beutel@web.de>
2019-03-30 23:25:11 +0100

    backed up files at Sa 30 Mär 2019 23:25:05 CET

--- plsql/backup-plsql-files/DIETPLAN/types/lebensmittel.sql
+++ plsql/backup-plsql-files/DIETPLAN/types/lebensmittel.sql
@@ -16,30 +16,3 @@ CREATE OR REPLACE TYPE "DIETPLAN"."LEBENSMITTEL" UNDER base_entity (
        ) RETURN SELF AS RESULT
 )
 /
-CREATE OR REPLACE TYPE BODY "DIETPLAN"."LEBENSMITTEL" AS
-    CONSTRUCTOR FUNCTION lebensmittel (
-        self IN OUT NOCOPY lebensmittel,
-        p_bezeichnung VARCHAR2
-    ) RETURN SELF AS RESULT IS
-    BEGIN
-        self := lebensmittel(p_bezeichnung, NULL, NULL, NULL);
-        return;
-    END;
-
-    CONSTRUCTOR FUNCTION lebensmittel (
-        self IN OUT NOCOPY lebensmittel,
-        p_bezeichnung    IN               VARCHAR2,
-        p_vitamin        IN               vitamin,
-        p_mineralstoff   IN               mineralstoff,
-        p_brennstoff     IN               brennstoff
-    ) RETURN SELF AS RESULT IS
-    BEGIN
-        self.v_bezeichnung := p_bezeichnung;
-        self.t_vitamin := p_vitamin;
-        self.t_mineralstoff := p_mineralstoff;
-        self.t_brennstoff := t_brennstoff;
-        return;
-    END lebensmittel;
-
-END;
-/

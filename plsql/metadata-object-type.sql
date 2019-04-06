c84242befe892b910ca217252495b667584d34d8
gituserDoesNotExist <manfred-beutel@web.de>
2019-04-06 17:59:08 +0200

    metadata

--- plsql/metadata-object-type.sql
+++ plsql/metadata-object-type.sql
@@ -1,22 +1,44 @@
-set serveroutput on;
+SET SERVEROUTPUT ON;
 
 -- select * from all_types where upper(type_name) = 'LEBENSMITTEL';
+
 DECLARE
+    dbobject_name   VARCHAR2(30) := 'LEBENSMITTEL';
     TYPE object_attributes IS
         TABLE OF VARCHAR2(50);
-    myattributes object_attributes := object_attributes();
-    myresult varchar2(10000) := '';
+    TYPE object_types IS
+        TABLE OF VARCHAR2(50);
+    myattributes    object_attributes := object_attributes();
+    mytypes         object_types := object_types();
+    myresult        VARCHAR2(10000) := '';
 BEGIN
-    SELECT attr_name BULK COLLECT
-    INTO myattributes
-    FROM all_type_attrs
-    WHERE upper(type_name) = 'LEBENSMITTEL';
+    SELECT object_type BULK COLLECT
+    INTO mytypes
+    FROM sys.all_objects
+    WHERE upper(owner) = 'DIETPLAN'
+          AND upper(object_name) = upper(dbobject_name);
 
-    FOR i IN 1..myattributes.last LOOP
-        myresult := concat(myresult,myattributes(i));
-        IF i != myattributes.last THEN
-            myresult := concat(myresult,';');
-        END IF;
-    END LOOP;
-    dbms_output.put_line(myresult);
+    IF mytypes IS EMPTY THEN
+        dbms_output.put_line('no;match;found');
+        return;
+    END IF;
+    
+    IF mytypes(1) LIKE 'TYPE%' THEN
+        dbms_output.put_line('it is a type');
+        SELECT attr_name BULK COLLECT
+        INTO myattributes
+        FROM all_type_attrs
+        WHERE upper(type_name) = upper(dbobject_name);
+
+        FOR i IN 1..myattributes.last LOOP
+            myresult := concat(myresult, myattributes(i));
+            IF i != myattributes.last THEN
+                myresult := concat(myresult, ';');
+            END IF;
+
+        END LOOP;
+        dbms_output.put_line(myresult);
+        return;
+    END IF;
+
 END;
--- plsql/metadata.sql
+++ plsql/metadata.sql
@@ -1,5 +1,9 @@
-select * from sys.all_objects where UPPER(owner) = 'DIETPLAN';
+select * from sys.all_objects where UPPER(owner) = 'DIETPLAN' and upper(object_name) = 'FOOD_DAO';
 
+select * from user_identifiers;
+select * from all_procedures where owner = 'DIETPLAN';
+select * from dba_procedures;
+select * from user_procedures;
 
 select * from sys.all_objects where UPPER(owner) = 'DIETPLAN' and upper(object_type) = 'TYPE';
 select * from sys.all_arguments where upper(object_name) = 'TEST_RUNNER';

9ec9db937b4228822df2d7e5aca72e23a4d81782
gituserDoesNotExist <manfred-beutel@web.de>
2019-04-08 21:11:33 +0200

    backed up files at Mo 08 Apr 2019 21:11:26 CEST

---
+++ additional-shit/backup-plsql-files/DIETPLAN/procedures/muypr.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE PROCEDURE "DIETPLAN"."MUYPR" (asssa in number)
+is
+begin
+null;
+end;
+/
---
+++ additional-shit/backup-plsql-files/DIETPLAN/procedures/myprocedure23333.sql
@@ -0,0 +1,5 @@
+CREATE OR REPLACE PROCEDURE "DIETPLAN"."MYPROCEDURE23333" (someparam in number) is
+begin
+null;
+end;
+/

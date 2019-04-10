b9ae8de9611291bb78f4738143ae7b017d9fdd6c
gituserDoesNotExist <manfred-beutel@web.de>
2019-04-10 21:28:31 +0200

    backed up files at Mi 10 Apr 2019 21:28:22 CEST

--- additional-shit/backup-plsql-files/DIETPLAN/procedures/myprocedure23333.sql
+++ additional-shit/backup-plsql-files/DIETPLAN/procedures/myprocedure23333.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE PROCEDURE "DIETPLAN"."MYPROCEDURE23333" (someparam in number) is
 begin
-dbms_output.put_line('hello worlsdfsdj;flsjflskjdf;sdlkfjs;kdfjs;dklfjd!!!!!');
+dbms_output.put_line('hello worlsdfsdj;flsjflskjdf;sdlkfjs;kdfjs;dklfjd!!!!!')
 end;
 /

935fd474d1862403b8611199ebb0428e09666b43
gituserDoesNotExist <manfred-beutel@web.de>
2019-04-24 23:36:17 +0200

    delete new_dao

--- plsql/DIETPLAN/packages/NEW_DAO.sql
+++
@@ -1,1 +0,0 @@
-CREATE OR REPLACE PACKAGE DIETPLAN.NEW_DAO AS  -- Gibt die ID des angelegten Lebensmittels zurueck FUNCTION create_lebensmittel ( t_lebensmittel IN OUT lebensmittel ) RETURN food_metadata.id%TYPE;  END NEW_DAO; /
